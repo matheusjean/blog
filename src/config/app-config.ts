e01917767ad3ce5913b86b3a6c4c7fd46384aeb5
matheusjean <matheusjean11@hotmail.com>
2021-08-23 16:52:57 -0300

    site url

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,4 +3,5 @@ export const API_URL = 'https://mysterious-stream-74021.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Matheus Jean';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+// export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://romantic-hodgkin-8688d0.netlify.app';
